5d33a3bfc09f3be45e75b22d9aff12ec24a424a3
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2025-10-24 21:39:19 +0200

    Autoformat everything
    Prettier for the README, shfmt for ykpgp.sh

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-yubikey-pgp
-===========
+# yubikey-pgp
 
 Easily use your Yubikey with `git` and `ssh` without manual configuration:
 yubikey-pgp is a wrapper for OpenPGP operations with your YubiKey without
@@ -13,31 +12,30 @@ If you put your binaries in `~/.local/bin`:
 
 ### Usage
 
-There are three main operations you might want to do with your
-YubiKey. First the least important: `ykpgp reset` resets all OpenPGP
-data on your YubiKey, but does not touch the other functions on it.
+There are three main operations you might want to do with your YubiKey. First
+the least important: `ykpgp reset` resets all OpenPGP data on your YubiKey, but
+does not touch the other functions on it.
 
-Then there's `init` and `register`. `init` is for when your YubiKey is
-new, and you want to make sure it is initialized with keys. The purpose
-of `register` is setting up your system to use keys that are already on
-your YubiKey. Setting up your system is also done by init, so you don't
-need to run `ykpgp register` after `ykpgp init`.
+Then there's `init` and `register`. `init` is for when your YubiKey is new, and
+you want to make sure it is initialized with keys. The purpose of `register` is
+setting up your system to use keys that are already on your YubiKey. Setting up
+your system is also done by init, so you don't need to run `ykpgp register`
+after `ykpgp init`.
 
 These two subcommands have a few options in common:
 
-  - `-n`        Use temporary GNUPGHOME. Mostly for testing
-  - `-i <uid>`  Add uid (e.g., `name <mail@example.com`) to key.
-    Can be specified multiple times. First is primary.
-    If none are given, default is "$NAME <$EMAIL>"
-  - `-g`        Set up open git repository for commit signing
-  - `-G`        Set up git for commit signing
-  - `-s`        Add key to possible ssh identities, and set up
-    your shell profile so ssh uses gpg.
+- `-n` Use temporary GNUPGHOME. Mostly for testing
+- `-i <uid>` Add uid (e.g., `name <mail@example.com`) to key. Can be specified
+  multiple times. First is primary. If none are given, default is
+  "$NAME <$EMAIL>"
+- `-g` Set up open git repository for commit signing
+- `-G` Set up git for commit signing
+- `-s` Add key to possible ssh identities, and set up your shell profile so ssh
+  uses gpg.
 
-`init` also has the option (for more advanced users) to use keys from
-the GPG keyring using `-k`. This almost invariably results in the actual
-key data being removed from the keyring, so if you go this route, make
-sure you have backups. `ykpgp` leaves the key data in place, but even
-a simple `gpg --card-status` will remove it. So if you want to have the
-same key data on multiple YubiKeys, immediately remove the YubiKey after
-initializing and initialize the next one.
+`init` also has the option (for more advanced users) to use keys from the GPG
+keyring using `-k`. This almost invariably results in the actual key data being
+removed from the keyring, so if you go this route, make sure you have backups.
+`ykpgp` leaves the key data in place, but even a simple `gpg --card-status`
+will remove it. So if you want to have the same key data on multiple YubiKeys,
+immediately remove the YubiKey after initializing and initialize the next one.
--- ykpgp.sh
+++ ykpgp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-die() { printf '%s\n' "$*" >&2; exit 1; }
+die() { printf '%s\n' "$*" >&2 && exit 1; }
 
 confirm() ( #1: prompt
     printf "%s [y/N] " "$1"
@@ -15,7 +15,7 @@ ykpgp_ensure_wsl_gpg() {
     grep -iq microsoft /proc/version 2>/dev/null || return 0
     #For the case when it's just installed
     if ! command -v gpg.exe >/dev/null 2>&1; then
-        printf "ERROR: missing gpg. Run `make deps` / `choco install gnupg`">&2
+        printf 'ERROR: missing gpg. Run `make deps`/`choco install gnupg`' >&2
         die "If you installed it this boot, restart your computer."
     fi
     gpg() { gpg.exe "$@"; }
@@ -27,13 +27,13 @@ ykpgp_ensure_wsl_gpg() {
 ykpgp_ensure_pinentry() {
     export GPG_TTY="${GPG_TTY-$(tty)}"
     if [ "$(uname -s)" = Darwin ] \
-            && ! gpgconf -X | grep -q pinentry-program \
-            && command -v pinentry-mac >/dev/null 2>&1; then
+        && ! gpgconf -X | grep -q pinentry-program \
+        && command -v pinentry-mac >/dev/null 2>&1; then
         #For some reason gpgconf does not do pinentry-program, both r/w
         set -- "$(mktemp)"
         {
             printf 'pinentry-program %s\n' "$(command -v pinentry-mac)"
-            cat "$GNUPGHOME/gpg-agent.conf" 2>/dev/null ||:
+            cat "$GNUPGHOME/gpg-agent.conf" 2>/dev/null || :
         } >"$1" && cp "$1" "$GNUPGHOME/gpg-agent.conf" && rm "$1"
     fi
 }
@@ -65,12 +65,15 @@ ykpgp_use_temp_gnupghome() {
     export GNUPGHOME="$(mktemp -d)"
     chmod og-rwx "$GNUPGHOME"
     gpg --list-keys >/dev/null 2>&1
-    exit_trap() { gpg --list-keys; rm -r "$GNUPGHOME"; }
+    exit_trap() {
+        gpg --list-keys
+        rm -r "$GNUPGHOME"
+    }
     trap exit_trap EXIT
 }
 
 ykpgp_gpg_commands() { #1: fingerprint or --card-edit
-    (shift 1; printf "%s\\n" "$@") \
+    (shift 1 && printf "%s\\n" "$@") \
         | gpg --command-fd=0 --status-fd=1 --expert \
             $([ "$1" = --card-edit ] || echo --key-edit) "$1"
 }
@@ -128,7 +131,7 @@ ykpgp_enable_git() { #1: git_config 2: fingerprint
     ykpgp_ensure_name
     git config "$1" commit.gpgsign true
     if ! echo "$uids" | grep -qxF \
-            "$(git config user.name) <$(git config user.email)>"; then
+        "$(git config user.name) <$(git config user.email)>"; then
         git config "$1" user.signingkey "$(ykpgp_get_gpg_keyid "$2")"
     fi
 }
@@ -137,7 +140,7 @@ ykpgp_enable_ssh() { #1: fingerprint
     gpgconf --list-options gpg-agent \
         | awk -F: '/^enable-ssh-support/ { exit ! $10 }' \
         || echo 'enable-ssh-support:1:1' | gpgconf --change-options gpg-agent \
-        >/dev/null
+            >/dev/null
     grip="$(gpg --with-colons --list-secret-keys "$1" | awk -F: '
         $1 ~ /s[us]b/ && $12 ~ /a/ { auth = 1 }
         $1 == "grp" && auth == 1 { print $10; exit }
@@ -219,16 +222,16 @@ ykpgp_init() {
     unset rsa
     while getopts 'gGi:knrs' OPT "$@"; do
         case "$OPT" in
-            g) git_config="--local" ;;
-            G) git_config="--global" ;;
-            i) uids="${uids-}$(printf "${uids+\\n}%s" "$OPTARG")" ;;
-            k) stored_keyring_key=true ;;
-            n) ykpgp_use_temp_gnupghome ;;
-            r) rsa=true ;;
-            s) enable_ssh=true ;;
+        g) git_config="--local" ;;
+        G) git_config="--global" ;;
+        i) uids="${uids-}$(printf "${uids+\\n}%s" "$OPTARG")" ;;
+        k) stored_keyring_key=true ;;
+        n) ykpgp_use_temp_gnupghome ;;
+        r) rsa=true ;;
+        s) enable_ssh=true ;;
         esac
     done
-    shift $(( $OPTIND - 1 )); OPTIND=1
+    shift $(($OPTIND - 1)) && OPTIND=1
     ykpgp_ensure_pinentry
     ykpgp_ensure_name
     pin_message="$(printf '%s\n' \
@@ -241,10 +244,11 @@ ykpgp_init() {
         '' \
         'After generating/copying the keys it will ask you to set' \
         'up new PINs for this YubiKey. Remember those.')"
-    passphrase_message="$(printf '%s\n' \
-        'If you already have a keypair, you will also be asked for' \
-        'its passphrase multiple times. Otherwise, make up' \
-        'something long and safe if you plan on saving the keypair.'
+    passphrase_message="$(
+        printf '%s\n' \
+            'If you already have a keypair, you will also be asked for' \
+            'its passphrase multiple times. Otherwise, make up' \
+            'something long and safe if you plan on saving the keypair.'
     )"
     if grep -iq microsoft /proc/version 2>/dev/null; then
         ykpgp_pinentry_message "$pin_message"
@@ -253,13 +257,13 @@ ykpgp_init() {
     else
         ykpgp_pinentry_message "$(echo "$pin_message" \
             && "${stored_keyring_key-false}" \
-            && echo && echo "$passphrase_message" ||:)"
+            && echo && echo "$passphrase_message" || :)"
     fi
     #Try setting up kdf. Not worth bothering the user over if card is not reset
-    ykpgp_gpg_commands --card-edit admin kdf-setup ||:
+    ykpgp_gpg_commands --card-edit admin kdf-setup || :
     #Splitting given and surname is imperfect, so only set if unset
     if gpg --with-colons --card-status | grep -qFx name:::; then
-        us="$(printf '\037')"; #ASCII Unit Separator
+        us="$(printf '\037')" #ASCII Unit Separator
         split_name="$(echo "$uids" | sed '
                 1!d
                 s/ <[^>]*>$//
@@ -284,12 +288,13 @@ ykpgp_init() {
                 "$("${rsa-false}" && echo rsa4096 || echo ed25519)" auth 0
         ykpgp_set_uids "$fingerprint"
         #What algorithms should the card be set to?
-        ykpgp_set_algo $(gpg --with-colons --list-keys "$fingerprint"|awk -F: '
-            /^[ps]ub:/ {
-                a[index($12, "sc") ? "s" : $12] = ($4 == 1 ? "rsa" $3 : $17)
-            }
-            END { print a["s"] " " a["e"] " " a["a"]; }
-        ')
+        ykpgp_set_algo $(gpg --with-colons --list-keys "$fingerprint" \
+            | awk -F: '
+                /^[ps]ub:/ {
+                    a[index($12, "sc") ? "s" : $12] = $4 == 1 ? "rsa" $3 : $17
+                }
+                END { print a["s"] " " a["e"] " " a["a"]; }
+            ')
         #Which subkey index do the [E] and [A] key have?
         order="$(gpg --with-colons --list-keys "$fingerprint" | awk -F: '
             /^sub:/ { c += 1; a[$12] = c; }
@@ -300,12 +305,12 @@ ykpgp_init() {
         cardstatus="$(gpg --with-colons --card-status)"
         ykpgp_gpg_commands "$fingerprint" \
             "key 0" keytocard y 1 \
-                $(echo "$cardstatus" | grep -xq 'fpr::[^:]*:[^:]*:' || echo y)\
+            $(echo "$cardstatus" | grep -xq 'fpr::[^:]*:[^:]*:' || echo y) \
             "key ${order% *}" keytocard 2 \
-                $(echo "$cardstatus" | grep -xq 'fpr:[^:]*::[^:]*:' || echo y)\
+            $(echo "$cardstatus" | grep -xq 'fpr:[^:]*::[^:]*:' || echo y) \
             "key 0" \
             "key ${order#* }" keytocard 3 \
-                $(echo "$cardstatus" | grep -xq 'fpr:[^:]*:[^:]*::' || echo y)
+            $(echo "$cardstatus" | grep -xq 'fpr:[^:]*:[^:]*::' || echo y)
         ykpgp_gpg_commands --card-edit admin passwd 1 3 Q
         #Delete key stubs to re add the private keys
         gpg --with-colons --list-secret-keys "$fingerprint" \
@@ -326,9 +331,9 @@ ykpgp_init() {
             | awk -F: '/^serial:/ { print $2 }')"
         ykpgp_gpg_commands --card-edit \
             admin generate n $replace 0 y \
-                "$(echo "$uids" | sed '1!d;s/ <[^>]*>$//')" \
-                "$(echo "$uids" | sed '1!d;s/.* <\([^>]*\)>$/\1/')" \
-                ""
+            "$(echo "$uids" | sed '1!d;s/ <[^>]*>$//')" \
+            "$(echo "$uids" | sed '1!d;s/.* <\([^>]*\)>$/\1/')" \
+            ""
         fingerprint="$(ykpgp_get_card_fingerprint "$serialno")"
         ykpgp_set_uids "$fingerprint"
         ykpgp_gpg_commands --card-edit admin passwd 1 3 Q
@@ -349,23 +354,24 @@ ykpgp() {
     export GNUPGHOME="${GNUPGHOME-$(gpgconf --list-dirs homedir)}"
     while getopts 'gGhi:ns' OPT "$@"; do
         case "$OPT" in
-            g) git_config="--local" ;;
-            G) git_config="--global" ;;
-            h) set -- help; OPTIND=1 ;;
-            i) uids="${uids-}$(printf "${uids+\\n}%s" "$OPTARG")" ;;
-            n) ykpgp_use_temp_gnupghome ;;
-            s) enable_ssh=true ;;
+        g) git_config="--local" ;;
+        G) git_config="--global" ;;
+        h) set -- help && OPTIND=1 ;;
+        i) uids="${uids-}$(printf "${uids+\\n}%s" "$OPTARG")" ;;
+        n) ykpgp_use_temp_gnupghome ;;
+        s) enable_ssh=true ;;
         esac
     done
-    shift $(( $OPTIND - 1 )); OPTIND=1
+    shift $(($OPTIND - 1))
+    OPTIND=1
     mkdir -p "$GNUPGHOME"
     chmod og-rwx "$GNUPGHOME"
     ykpgp_ensure_wsl_gpg
     case "$1" in
-        help) shift 1; ykpgp_help ;;
-        register) shift 1; ykpgp_register "$@" ;;
-        init) shift 1; ykpgp_init "$@" ;;
-        reset) shift 1; ykpgp_reset "$@" ;;
+    help) shift 1 && ykpgp_help ;;
+    register) shift 1 && ykpgp_register "$@" ;;
+    init) shift 1 && ykpgp_init "$@" ;;
+    reset) shift 1 && ykpgp_reset "$@" ;;
     esac
 }
 
